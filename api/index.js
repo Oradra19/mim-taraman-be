062561c89b21e09f5afb4e35300586b27cf6b127
Oradra <ogyarjndra@gmail.com>
2026-01-03 01:18:38 +0700

    coba koneksi db

--- api/index.js
+++ api/index.js
@@ -1,2 +1,4 @@
 const app = require("../src/app");
+
+// Vercel akan otomatis handle request ke /api
 module.exports = app;
--- src/app.js
+++ src/app.js
@@ -4,30 +4,30 @@ const cors = require("cors");
 
 const app = express();
 
+// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// TEST ROOT (akan muncul di /api)
 app.get("/", (req, res) => {
   res.send("API MIM School running 🚀");
 });
 
-// ROUTES
-app.use("/api/auth", require("./routes/auth.routes"));
-app.use("/api/banner", require("./routes/banner.routes"));
-app.use("/api/informasi", require("./routes/informasi.routes"));
-app.use("/api/iklan", require("./routes/iklan.routes"));
-app.use("/api/sambutan", require("./routes/sambutan.routes"));
-app.use("/api/data-sekolah", require("./routes/dataSekolah.routes"));
-app.use("/api/prestasi", require("./routes/prestasi.routes"));
-app.use("/api/ekskul", require("./routes/ekskul.routes"));
-app.use("/api/berita", require("./routes/berita.routes"));
-app.use("/api/galeri-foto", require("./routes/galeriFoto.routes"));
-app.use("/api/galeri-video", require("./routes/galeriVideo.routes"));
-app.use("/api/kuesioner", require("./routes/kuesioner.routes"));
-app.use("/api/saran", require("./routes/saran.routes"));
-app.use("/api/media-sosial", require("./routes/mediaSosial.routes"));
-
-
+// ROUTES (TANPA /api PREFIX)
+app.use("/auth", require("./routes/auth.routes"));
+app.use("/banner", require("./routes/banner.routes"));
+app.use("/informasi", require("./routes/informasi.routes"));
+app.use("/iklan", require("./routes/iklan.routes"));
+app.use("/sambutan", require("./routes/sambutan.routes"));
+app.use("/data-sekolah", require("./routes/dataSekolah.routes"));
+app.use("/prestasi", require("./routes/prestasi.routes"));
+app.use("/ekskul", require("./routes/ekskul.routes"));
+app.use("/berita", require("./routes/berita.routes"));
+app.use("/galeri-foto", require("./routes/galeriFoto.routes"));
+app.use("/galeri-video", require("./routes/galeriVideo.routes"));
+app.use("/kuesioner", require("./routes/kuesioner.routes"));
+app.use("/saran", require("./routes/saran.routes"));
+app.use("/media-sosial", require("./routes/mediaSosial.routes"));
 
 module.exports = app;
