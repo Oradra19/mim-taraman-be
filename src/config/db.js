3675abe035f1cc36e09697c8bd980edc72f85911
Oradra <ogyarjndra@gmail.com>
2026-01-03 01:14:31 +0700

    coba koneksi db

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
+  port: process.env.MYSQL_ADDON_PORT,
 });
+
+console.log("DB HOST:", process.env.MYSQL_ADDON_HOST);
 
 module.exports = db;
