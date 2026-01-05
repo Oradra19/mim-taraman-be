c102064f08fa0468a003931ae87fd7c1d1e42c75
Oradra <ogyarjndra@gmail.com>
2026-01-05 22:18:01 +0700

    rubah db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
   host: process.env.MYSQL_ADDON_HOST,
@@ -7,9 +7,11 @@ const db = mysql.createPool({
   password: process.env.MYSQL_ADDON_PASSWORD,
   database: process.env.MYSQL_ADDON_DB,
   port: process.env.MYSQL_ADDON_PORT,
+  waitForConnections: true,
+  connectionLimit: 5, // SAMA dengan max DB
+  queueLimit: 0,
 });
 
 console.log("DB HOST:", process.env.MYSQL_ADDON_HOST);
 
 module.exports = db;
-orts = db;
